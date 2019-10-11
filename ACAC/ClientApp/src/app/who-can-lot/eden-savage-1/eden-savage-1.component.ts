c753c47b5cb8dabb4e2a83409339c910c2bc0f45
opedrajas <opedrajas@OPEDRAJAS-408>
2019-10-11 17:01:31 -0400

    updates

--- ACAC/ClientApp/src/app/who-can-lot/eden-savage-1/eden-savage-1.component.ts
+++ ACAC/ClientApp/src/app/who-can-lot/eden-savage-1/eden-savage-1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-eden-savage-1',
@@ -12,7 +13,11 @@ export class EdenSavage1Component {
   displayedColumns: string[] = ['raiditem'];
   displayedColumns1: string[] = ['dateReceived', 'floor', 'raider', 'droptype', 'id'];
   EquipmentItems: any[];
-  SavageItems: any[];
+  SavageItems: any[] ;
+  previousValue: string;
+
+  profiles: any[] = [''];
+  itemsbyProfile: any[] = new Array();
 
   constructor(private http: HttpClient) {
     const baseUrl = document.getElementsByTagName('base')[0].href;
@@ -21,7 +26,23 @@ export class EdenSavage1Component {
      }, error => console.error(error));
 
     http.get<any[]>(baseUrl + 'api/ACAC/GetRaidItemsByFloor?XFloor=Eden Savage Floor 1').subscribe(result => {
-      this.SavageItems = result;
+      // console.log(result);
+      this.SavageItems = result.sort((a, b) => (a.receiveddate < b.receiveddate) ? 1 : -1);
+
+      result.sort((a, b) => (a.raidername > b.raidername) ? 1 : -1).forEach((value) => {
+          // console.log(value);
+          if (this.previousValue !== value.raidername) {
+            this.profiles.push(value.raidername);
+            this.previousValue = value.raidername;
+          }
+      });
+
+      this.profiles.forEach((value) => {
+        if (value !== '') {
+          this.itemsbyProfile.push(result.filter(r => r.raidername === value).sort((a, b) => (a.receiveddate < b.receiveddate) ? 1 : -1));
+        }
+      });
+
     }, error => console.error(error));
   }
 }
--- ACAC/ClientApp/src/app/who-can-lot/eden-savage-2/eden-savage-2.component.ts
+++ ACAC/ClientApp/src/app/who-can-lot/eden-savage-2/eden-savage-2.component.ts
@@ -16,6 +16,14 @@ export class EdenSavage2Component {
   history1: any[];
   history2: any[];
 
+  profiles1: any[] = [''];
+  profiles2: any[] = [''];
+
+  previousValue: any = '';
+
+  itemsbyProfile: any[] = new Array();
+  itemsbyProfile2: any[] = new Array();
+
   constructor(private http: HttpClient) {
     const baseUrl = document.getElementsByTagName('base')[0].href;
     http.get<any[]>(baseUrl + 'api/ACAC/GetRoundRobinList?XRaidfloorname=Eden Savage Floor 2').subscribe(result => {
@@ -28,7 +36,32 @@ export class EdenSavage2Component {
     http.get<any[]>(baseUrl + 'api/ACAC/GetRaidItemsByFloor?XFloor=Eden Savage Floor 2').subscribe(result => {
 
       this.history1 = result.filter(r => r.raidItem === 'Equipment Coffer').sort((a, b) => (a.receiveddate < b.receiveddate) ? 1 : -1);
+      this.history1.sort((a, b) => (a.raidername > b.raidername) ? 1 : -1).forEach((value) => {
+          if (this.previousValue !== value.raidername) {
+            this.profiles1.push(value.raidername);
+            this.previousValue = value.raidername;
+          }
+      });
+      this.profiles1.forEach((value) => {
+        if (value !== '') {
+          // tslint:disable-next-line: max-line-length
+          this.itemsbyProfile.push(this.history1.filter(r => r.raidername === value).sort((a, b) => (a.receiveddate < b.receiveddate) ? 1 : -1));
+        }
+      });
+      this.previousValue = '';
       this.history2 = result.filter(r => r.raidItem === 'Deepshadow Coating').sort((a, b) => (a.receiveddate < b.receiveddate) ? 1 : -1);
+      this.history2.sort((a, b) => (a.raidername > b.raidername) ? 1 : -1).forEach((value) => {
+        if (this.previousValue !== value.raidername) {
+          this.profiles2.push(value.raidername);
+          this.previousValue = value.raidername;
+        }
+      });
+      this.profiles2.forEach((value) => {
+        if (value !== '') {
+          // tslint:disable-next-line: max-line-length
+          this.itemsbyProfile2.push(this.history2.filter(r => r.raidername === value).sort((a, b) => (a.receiveddate < b.receiveddate) ? 1 : -1));
+        }
+      });
 
     }, error => console.error(error));
   }
