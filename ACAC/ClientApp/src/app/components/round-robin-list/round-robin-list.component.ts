03e8b4a15e09834c79bcce687b0fe58ac14b2500
sanoken <sanoken@live.com>
2019-10-21 23:55:56 -0400

    component updates

--- ACAC/ClientApp/src/app/components/round-robin-list/round-robin-list.component.ts
+++ ACAC/ClientApp/src/app/components/round-robin-list/round-robin-list.component.ts
@@ -18,71 +18,44 @@ export class RoundRobinListComponent implements OnInit, OnChanges {
 
     ngOnInit() {}
     ngOnChanges() {
-        console.log('ngOnChanges - ' + this.Floorname);
+
         const baseUrl = document.getElementsByTagName('base')[0].href;
         this.roundrobinlists = [];
 
-        let roundrobinlist: RoundRobinList;
         this.http.get<any[]>(baseUrl + 'api/ACAC/GetRoundRobinList?XRaidfloorname=' + this.Floorname).subscribe(result => {
 
             switch (this.Floorname) {
                 case 'Eden Savage Floor 1': {
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Accessory Coffer';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Accessory Coffer');
-                    this.roundrobinlists.push(roundrobinlist);
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Accessory Coffer'));
                     break;
                 }
                 case 'Eden Savage Floor 2': {
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Equipment Coffer';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Equipment Coffer');
-                    this.roundrobinlists.push(roundrobinlist);
-
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Deepshadow Coating';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Deepshadow Coating');
-                    this.roundrobinlists.push(roundrobinlist);
-
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Equipment Coffer'));
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Deepshadow Coating'));
                     break;
                 }
                 case 'Eden Savage Floor 3': {
-
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Equipment Coffer';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Equipment Coffer');
-                    this.roundrobinlists.push(roundrobinlist);
-
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Deepshadow Coating';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Deepshadow Twine');
-                    this.roundrobinlists.push(roundrobinlist);
-
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Deepshadow Solvent';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Deepshadow Solvent');
-                    this.roundrobinlists.push(roundrobinlist);
-
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Equipment Coffer'));
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Deepshadow Twine'));
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Deepshadow Solvent'));
                     break;
                 }
                 case 'Eden Savage Floor 4': {
-
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Chest Coffer';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Chest Coffer');
-                    this.roundrobinlists.push(roundrobinlist);
-
-                    roundrobinlist = new RoundRobinList();
-                    roundrobinlist.listname = 'Weapon Coffer';
-                    roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === 'Weapon Coffer');
-                    this.roundrobinlists.push(roundrobinlist);
-
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Chest Coffer'));
+                    this.roundrobinlists.push(this.Generateroundrobinlist(result, 'Weapon Coffer'));
                     break;
                 }
             }
-            console.log(this.roundrobinlists);
+
         }, error => console.error(error));
 
     }
     constructor(private http: HttpClient) {}
+
+    Generateroundrobinlist(result, listname) {
+        const roundrobinlist = new RoundRobinList();
+        roundrobinlist.listname = listname;
+        roundrobinlist.EquipmentItems = result.filter(r => r.raiditem === listname);
+        return roundrobinlist;
+    }
 }
